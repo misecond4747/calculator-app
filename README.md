ef837bd9e957ba836c9f539f8b65cba9f034be65
misecond4747 <149604633+misecond4747@users.noreply.github.com>
2024-09-08 14:31:36 +0500

    final touches

--- README.md
+++ README.md
@@ -35,8 +35,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://github.com/misecond4747/calculator-app.git)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [https://github.com/misecond4747/calculator-app.git]
+- Live Site URL: [https://calculator-app-nine-dusky.vercel.app/]
 
 ## My process
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
 import Calculator from "@/components/Calculator";
+import Link from "next/link";
 
 export default function Home() {
-  return <Calculator />;
+  return (
+    <>
+      <Calculator />{" "}
+      <div className="attribution">
+        Challenge by
+        <Link
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </Link>
+        . Coded by
+        <Link
+          href="
+  https://github.com/misecond4747
+  "
+        >
+          M Ibrahim
+        </Link>
+        .
+      </div>
+    </>
+  );
 }
